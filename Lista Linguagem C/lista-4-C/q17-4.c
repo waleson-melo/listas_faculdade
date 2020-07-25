d8af8d7b037aa43cb082037a7cbc495f9ce78762
Waleson Melo <walesonmelo23@gmail.com>
2020-07-25 19:43:58 -0400

    Create q17-4.c

---
+++ Lista Linguagem C/lista-4-C/q17-4.c
@@ -0,0 +1,9 @@
+/* Questão 17. Faça um programa, utilizando estrutura de condição, que receba um número real, digitado pelo
+usuário e mostre o menu para selecionar o tipo de cálculo que deve ser realizado. No final, mostre o resultado e
+a operação efetuada.
+MENU
+101-Raiz quadrada
+102-A metade
+103-10% do número
+104-O dobro
+Escolha a opção: */
