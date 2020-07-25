4fdc33ea19a2067042c871370545bd79bc61b5bd
Waleson Melo <walesonmelo23@gmail.com>
2020-07-25 19:44:48 -0400

    Create q20-5.c

---
+++ Lista Linguagem C/lista-5-C/q20-5.c
@@ -0,0 +1,15 @@
+/* Questão 20. Implemente um algoritmo para calcular o resultado da série abaixo. Os valores de n e x deverão ser
+digitados pelo usuário. O programa deve mostrar os elementos que compõem a série e a soma dos elementos. */
+#include <stdio.h>
+int main(){
+    int n, x;
+    
+    printf("n: ");
+    scanf("%d", &n);
+	printf("x: ");
+    scanf("%d", &x);
+    
+    
+	
+    return 0;
+}
