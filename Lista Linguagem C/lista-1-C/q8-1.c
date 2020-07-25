10053eb33546fe6027b1c42dcf9525da9c5af3c2
Waleson Melo <walesonmelo23@gmail.com>
2020-07-25 19:41:24 -0400

    Create q8-1.c

---
+++ Lista Linguagem C/lista-1-C/q8-1.c
@@ -0,0 +1,11 @@
+/* Questão 8. Calcule o consumo médio de um automóvel sendo fornecidos a distância total
+percorrida (em Km) e o total de combustível gasto (em litros). A entrada contém dois valores: um
+valor inteiro D representando a distância total percorrida (em Km), e um valor real C representando
+o total de combustível gasto, com um dígito após o ponto decimal. Apresente o valor que
+representa o consumo médio do automóvel, seguido da mensagem "km/l". */
+#include <stdio.h>
+int main(){
+    
+
+    return 0;
+}
