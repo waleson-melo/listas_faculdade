ce2a6e87dcdf6e97a97dd7c8d4a46a3d11f24a6b
Waleson Melo <walesonmelo23@gmail.com>
2020-09-17 12:43:48 -0400

    Create q3.c

---
+++ Lista Linguagem C/2-Periodo/lista-1-C/q3.c
@@ -0,0 +1,34 @@
+/*
+    Questão 3. Crie um programa que preencha um vetor de 50 posições, depois multiplique cada
+valor do vetor por um valor inteiro lido do usuario, mantendo os novos valores no mesmo vetor. Mostre o resultado.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define T 50
+
+int main()
+{
+    int vet[T], i, num;
+
+    // Preenchendo o vetor com valores aleatorios
+    for(i = 0; i < T; i++)
+        vet[i] = rand() % 100;
+
+    // Mostrando os valores do vetor
+    printf("Valores do Vetor:\n");
+    for(i = 0; i < T; i++)
+        printf("%d\t", vet[i]);
+
+    // Multiplicando os valores do vetor pelo numero digitado pelo usuario
+    printf("\nPor quanto voce deseja multiplicar os valores do vetor?\t");
+    scanf("%d", &num);
+    for(i = 0; i < T; i++)
+        vet[i] = vet[i] * num;
+
+    // Mostrando os novos valores
+    printf("Novos valores do vetor:\n");
+    for(i = 0; i < T; i++)
+        printf("%d\t", vet[i]);
+}
