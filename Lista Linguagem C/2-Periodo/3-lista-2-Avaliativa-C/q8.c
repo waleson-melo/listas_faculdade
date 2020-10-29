011865399f2f7eeb6dcdc7fb002d4f049e1a5b53
Waleson Melo <walesonmelo23@gmail.com>
2020-10-28 21:28:07 -0400

    Update q8.c
    deixando mais organizado
    comentando para melhor entendimento

--- Lista Linguagem C/2-Periodo/3-lista-2-Avaliativa-C/q8.c
+++ Lista Linguagem C/2-Periodo/3-lista-2-Avaliativa-C/q8.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 
 typedef struct endereco{
-    char logradouro[50];
+    char logradouro[50], bairro[30];
     int numero;
-    char bairro[30];
     long int cep;
 }Endereco;
 
@@ -35,9 +34,8 @@ int main()
     scanf("%ld", &cliente.endereco.cep);
 
     printf("===========DADOS============\n");
-    printf("Matricula: %d\nNome: %s\n", cliente.matricula, cliente.nome);
-    printf("Logradouro: %s\nNumero: %d\nBairro: %s\nCep: %ld\n", 
-        cliente.endereco.logradouro, cliente.endereco.numero, 
+    printf("Matricula: %d\nNome: %s\nLogradouro: %s\nNumero: %d\nBairro: %s\nCep: %ld\n", 
+        cliente.matricula, cliente.nome, cliente.endereco.logradouro, cliente.endereco.numero, 
         cliente.endereco.bairro, cliente.endereco.cep);
 
     return 0;
