4a0a7e9db7c24176d026f54e13b965e811d3ba8a
Waleson Melo <walesonmelo23@gmail.com>
2020-10-28 21:27:25 -0400

    Update q11.c
    deixando mais organizado
    comentando para melhor entendimento

--- Lista Linguagem C/2-Periodo/3-lista-2-Avaliativa-C/q11.c
+++ Lista Linguagem C/2-Periodo/3-lista-2-Avaliativa-C/q11.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 
-#define T 5
+#define T 5     // Tamanho do vetor eletro
 
 typedef struct eletro{
     char nome[15];
-    float potencia;
-    float tempo;
+    float potencia, tempo;
 }Eletro;
 
 int main()
@@ -28,6 +27,7 @@ int main()
     printf("Dias: ");
     scanf("%d", &t);
 
+    // Consumo total
     for(i = 0; i < T; i++){
         soma += eletro[i].potencia * eletro[i].tempo;
     }
@@ -36,8 +36,10 @@ int main()
     printf("=========DADOS=========\n");
     printf("Consumo total: %0.2f\n", consumoTotal);
     printf("-------------\n");
+
+    // Consumo relativo
     for(i = 0; i < T; i++){
-        consumoRelativo = (((eletro[i].potencia * eletro[i].tempo) * t) / consumoTotal) * 100;
+        consumoRelativo = (((eletro[i].potencia * eletro[i].tempo) * t) * 100) / consumoTotal;
         printf("Nome: %s\nConsumo relativo: %0.2f%%\n",
             eletro[i].nome, consumoRelativo);
         printf("-------------------------\n");
