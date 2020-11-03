7031fa96f27d147ac3d758a32e3a194d03350ad4
Waleson Melo <walesonmelo23@gmail.com>
2020-11-03 12:25:50 -0400

    Update q13.c

--- Lista Linguagem C/2-Periodo/3-lista-2-Avaliativa-C/q13.c
+++ Lista Linguagem C/2-Periodo/3-lista-2-Avaliativa-C/q13.c
@@ -1,88 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define T 3
+#define T 3        // Tamanho do vetor da fila de atendimento
 
 typedef struct paciente{
     int cpf;
     int idade;
 }Paciente;
 
-void inserirPaciente(Paciente fila[T], int *quant, int *numPacAte, int *inicio);
-void mostrarFila(Paciente fila[T], int quant);
+void inserirPaciente(Paciente fila[T], int *numPac, int *quantAte);
+void mostrarFila(Paciente fila[T], int numPac);
 
 int main()
 {
     Paciente fila[T];
-    int numPacAte = 0, i = 1, op, quant = 0, inicio = quant;
+    int numPac = 0, i = 1, quantAte = 0, op;    // quantAte é o numero de pacientes atendidos
 
     while(i != 0){
-        mostrarFila(fila, quant);
 
-        printf("1-Inserir Paciente\n2-Retirar Paciente\nOpcao: ");
-        scanf("%d", &op);
-
-        switch(op){
-            case 1:
-                printf("=====INSERIR=====\n");
-                inserirPaciente(fila, &quant, &numPacAte, &inicio);
-                break;
-            default:
-                printf("OPCAO INVALIDA\n\n");
-                break;
+        if(quantAte < T){
+            printf("==========Inserir==========\n");
+            inserirPaciente(fila, &numPac, &quantAte);
+            mostrarFila(fila, numPac);
+        } else{
+            i = 0;
         }
     }
-
 
     return 0;
 }
 
 // FUNÇÔES
-void inserirPaciente(Paciente fila[T], int *quant, int *numPacAte, int *inicio)
+void inserirPaciente(Paciente fila[T], int *numPac, int *quantAte)
 {
-    int idade, i, pos;
+    if(*quantAte <= T){
+        int pos = *numPac, idade, i;
 
-    if((*quant < T) || (*numPacAte < T)){
         printf("Idade: ");
         scanf("%d", &idade);
 
+        // Verificando a posicao onde ele vai ficar no vetor caso a idade seja maior ou igual a 60
+        // se nao for os dados vao para a direita e adiciona o paciente no final da fila
         if(idade >= 60){
-            for(i = *quant; i >= *inicio; i--){
-                pos = i;
+            for(i = *numPac-1; i >= 0; i--){
+                if((fila[i].idade >= 60) && (idade < fila[i].idade)){
+                    fila[i+1] = fila[i];
+                    pos = i;
+                } 
             }
             fila[pos].idade = idade;
             printf("CPF: ");
             scanf("%d", &fila[pos].cpf);
-            *inicio += 1;
-            *quant += 1;
+            *numPac += 1;
+            *quantAte += 1;
         } else{
-            for(i = *quant; i >= 0; i--){
+            pos = 0;
+            for(i = *numPac-1; i >= 0; i--){
                 fila[i+1] = fila[i];
             }
-            fila[0].idade = idade;
-            setbuf(stdin, NULL);
+            fila[pos].idade = idade;
             printf("CPF: ");
-            scanf("%d", &fila[0].cpf);
-            *quant += 1;
-            *numPacAte += 1;
-            system("clear");
+            scanf("%d", &fila[pos].cpf);
+            *numPac += 1;
+            *quantAte += 1;
         }
+        // Se estiver no windows troque para system("cls")
+        system("clear");
     } else{
-        printf("FILA CHEIA\n");
+        printf("CHEIO\n");
     }
 }
 
-void mostrarFila(Paciente fila[T], int quant)
+void mostrarFila(Paciente fila[T], int numPac)
 {
     int i;
     printf("=====FILA=====\n");
-    if(quant > 0){
-        for(i = quant-1; i >= 0; i--){
+    if(numPac > 0){
+        for(i = numPac-1; i >= 0; i--){
             printf("CPF: %d\nIdade: %d\n", fila[i].cpf, fila[i].idade);
             printf("-----------------------\n");
         }
     } else{
         printf("FILA VAZIA!!!\n");
-        printf("-----------------------\n");
     }
 }
